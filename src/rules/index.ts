17336b341fec226f815b9667a4744969a67cddae
Susisu <susisu2413@yahoo.co.jp>
2022-03-21 21:23:40 +0900

    fix: fix no-object-assign rule was not exported

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,8 +1,10 @@
+import noObjectAssign from "./no-object-assign";
 import noUnsafeObjectEnumMethod from "./no-unsafe-object-enum-method";
 import noUnsafeObjectPropertyCheck from "./no-unsafe-object-property-check";
 import noUnsafeObjectPropertyOverwrite from "./no-unsafe-object-property-overwrite";
 
 export default {
+  "no-object-assign": noObjectAssign,
   "no-unsafe-object-enum-method": noUnsafeObjectEnumMethod,
   "no-unsafe-object-property-check": noUnsafeObjectPropertyCheck,
   "no-unsafe-object-property-overwrite": noUnsafeObjectPropertyOverwrite,
