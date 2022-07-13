80bc91cf568425cdd7e3e742aac9bfadb554dc1b
Susisu <susisu2413@yahoo.co.jp>
2022-07-13 21:44:01 +0900

    feat: add no-type-assertion to recommended config

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -1,6 +1,7 @@
 export default {
   rules: {
     "@susisu/safe-typescript/no-object-assign": "error",
+    "@susisu/safe-typescript/no-type-assertion": "error",
     "@susisu/safe-typescript/no-unsafe-object-enum-method": "error",
     "@susisu/safe-typescript/no-unsafe-object-property-check": "error",
     "@susisu/safe-typescript/no-unsafe-object-property-overwrite": "error",
