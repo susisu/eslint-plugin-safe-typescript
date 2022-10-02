786cb915689fa2c918f44ad352d92261ec1aa8d2
Susisu <susisu2413@yahoo.co.jp>
2022-10-02 17:26:02 +0900

    chore: update ts-jest config

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,12 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ["./src/**/*.ts", "!./src/**/*.spec.ts", "!./src/**/__tests__/**/*.ts"],
   coverageDirectory: "coverage",
-  globals: {
-    "ts-jest": {
-      tsconfig: "./tsconfig.test.json",
-    },
-  },
   transform: {
-    "\\.tsx?$": "ts-jest",
+    "\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "./tsconfig.test.json",
+      },
+    ],
   },
 };
