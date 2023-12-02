bb3b01ac3d25b97d8ef613886a4d0705bdd6c2f7
Susisu <susisu2413@yahoo.co.jp>
2023-12-02 22:25:01 +0900

    chore: migrate to ESM

---
+++ eslint.config.js
@@ -0,0 +1,72 @@
+import { config, map } from "@susisu/eslint-config";
+import prettierConfig from "eslint-config-prettier";
+import eslintPluginPlugin from "eslint-plugin-eslint-plugin";
+import vitestPlugin from "eslint-plugin-vitest";
+import globals from "globals";
+
+export default [
+  ...map(
+    {
+      files: ["src/**/*.ts"],
+    },
+    [
+      config.tsTypeChecked(),
+      {
+        plugins: {
+          "eslint-plugin": eslintPluginPlugin,
+        },
+        languageOptions: {
+          sourceType: "module",
+          parserOptions: {
+            project: "./tsconfig.json",
+          },
+          globals: {
+            ...globals.es2021,
+            ...globals.node,
+          },
+        },
+        rules: {
+          ...eslintPluginPlugin.configs.recommended.rules,
+        },
+      },
+    ],
+  ),
+  ...map(
+    {
+      files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"],
+    },
+    [
+      {
+        plugins: {
+          vitest: vitestPlugin,
+        },
+        languageOptions: {
+          globals: {
+            ...vitestPlugin.environments.env.globals,
+          },
+        },
+        rules: {
+          ...vitestPlugin.configs.recommended.rules,
+        },
+      },
+    ],
+  ),
+  ...map(
+    {
+      files: ["*.js"],
+    },
+    [
+      config.js(),
+      {
+        languageOptions: {
+          sourceType: "module",
+          globals: {
+            ...globals.es2021,
+            ...globals.node,
+          },
+        },
+      },
+    ],
+  ),
+  prettierConfig,
+];
