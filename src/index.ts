336b12c02e90bcafbbab8d87ca8ef79205ec93a2
Susisu <susisu2413@yahoo.co.jp>
2023-12-16 21:03:14 +0900

    feat: add default export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- fix TS2742 error
+import type { ESLintUtils } from "@typescript-eslint/utils";
 import rules from "./rules";
 import configs from "./configs";
 
 export { rules, configs };
+export default { rules, configs };
