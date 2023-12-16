934d68bc94b295fbf951530affdf3419113c10c6
Susisu <susisu2413@yahoo.co.jp>
2023-12-16 20:57:31 +0900

    style: reformat

--- eslint.config.js
+++ eslint.config.js
@@ -5,68 +5,53 @@ import vitestPlugin from "eslint-plugin-vitest";
 import globals from "globals";
 
 export default [
-  ...map(
+  ...map({ files: ["src/**/*.ts"] }, [
+    config.tsTypeChecked(),
     {
-      files: ["src/**/*.ts"],
-    },
-    [
-      config.tsTypeChecked(),
-      {
-        plugins: {
-          "eslint-plugin": eslintPluginPlugin,
+      plugins: {
+        "eslint-plugin": eslintPluginPlugin,
+      },
+      languageOptions: {
+        sourceType: "module",
+        parserOptions: {
+          project: "./tsconfig.json",
         },
-        languageOptions: {
-          sourceType: "module",
-          parserOptions: {
-            project: "./tsconfig.json",
-          },
-          globals: {
-            ...globals.es2021,
-            ...globals.node,
-          },
-        },
-        rules: {
-          ...eslintPluginPlugin.configs.recommended.rules,
+        globals: {
+          ...globals.es2021,
+          ...globals.node,
         },
       },
-    ],
-  ),
-  ...map(
-    {
-      files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"],
+      rules: {
+        ...eslintPluginPlugin.configs.recommended.rules,
+      },
     },
-    [
-      {
-        plugins: {
-          vitest: vitestPlugin,
-        },
-        languageOptions: {
-          globals: {
-            ...vitestPlugin.environments.env.globals,
-          },
-        },
-        rules: {
-          ...vitestPlugin.configs.recommended.rules,
+  ]),
+  ...map({ files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"] }, [
+    {
+      plugins: {
+        vitest: vitestPlugin,
+      },
+      languageOptions: {
+        globals: {
+          ...vitestPlugin.environments.env.globals,
         },
       },
-    ],
-  ),
-  ...map(
-    {
-      files: ["*.js"],
+      rules: {
+        ...vitestPlugin.configs.recommended.rules,
+      },
     },
-    [
-      config.js(),
-      {
-        languageOptions: {
-          sourceType: "module",
-          globals: {
-            ...globals.es2021,
-            ...globals.node,
-          },
+  ]),
+  ...map({ files: ["*.js"] }, [
+    config.js(),
+    {
+      languageOptions: {
+        sourceType: "module",
+        globals: {
+          ...globals.es2021,
+          ...globals.node,
         },
       },
-    ],
-  ),
+    },
+  ]),
   prettierConfig,
 ];
