a20380f8e39fb1f0adeff74d127706b0f6b7db07
Susisu <susisu2413@yahoo.co.jp>
2024-02-10 16:30:56 +0900

    chore: upgrade eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,57 +1,45 @@
-import { config, map } from "@susisu/eslint-config";
-import prettierConfig from "eslint-config-prettier";
+import { make } from "@susisu/eslint-config";
 import eslintPluginPlugin from "eslint-plugin-eslint-plugin";
 import vitestPlugin from "eslint-plugin-vitest";
 import globals from "globals";
 
-export default [
-  ...map({ files: ["src/**/*.ts"] }, [
-    config.tsTypeChecked(),
-    {
-      plugins: {
-        "eslint-plugin": eslintPluginPlugin,
-      },
-      languageOptions: {
-        sourceType: "module",
-        parserOptions: {
-          project: true,
-        },
-        globals: {
-          ...globals.es2021,
-          ...globals.node,
-        },
-      },
-      rules: {
-        ...eslintPluginPlugin.configs.recommended.rules,
+export default make({}, [
+  {
+    plugins: {
+      "eslint-plugin": eslintPluginPlugin,
+      vitest: vitestPlugin,
+    },
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
       },
     },
-  ]),
-  ...map({ files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"] }, [
-    {
-      plugins: {
-        vitest: vitestPlugin,
-      },
-      languageOptions: {
-        globals: {
-          ...vitestPlugin.environments.env.globals,
-        },
-      },
-      rules: {
-        ...vitestPlugin.configs.recommended.rules,
+    rules: {
+      ...eslintPluginPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...vitestPlugin.environments.env.globals,
       },
     },
-  ]),
-  ...map({ files: ["*.js"] }, [
-    config.js(),
-    {
-      languageOptions: {
-        sourceType: "module",
-        globals: {
-          ...globals.es2021,
-          ...globals.node,
-        },
+    rules: {
+      ...vitestPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
       },
     },
-  ]),
-  prettierConfig,
-];
+  },
+]);
